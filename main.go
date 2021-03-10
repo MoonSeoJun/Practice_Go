4b893ec9cf2fbb129b1f765e513404b12827c271
MunSeoJun <msj00130@gmail.com>
2021-03-10 22:47:13 +0900

    Marshal JSON

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// User for json marshaling
+type User struct {
+	Name   string `json:"name"`
+	Age    int    `json:"age"`
+	Gender string `json:"gender"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/sayHello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	}).Methods("GET")
+	r.HandleFunc("/method", JSONMarshal).Methods("POST")
+	http.ListenAndServe(":8080", r)
+}
+
+// JSONMarshal is marshaling struct
+func JSONMarshal(w http.ResponseWriter, r *http.Request) {
+	u := User{}
+
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		panic(err)
+	}
+
+	userJSON, err := json.Marshal(u)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(userJSON)
+}
