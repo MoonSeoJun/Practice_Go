85c771e7e7ab633410ddebc477a7fe4c4b1a6999
MunSeoJun <msj00130@gmail.com>
2021-03-11 13:05:53 +0900

    [Deleted] server file deleted

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/MoonSeoJun/FreeWebMaking/controllers"
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	r := mux.NewRouter()
 
 	Apis(r)
 	RunServer(":8080", r)
+}
+
+// RunServer - using https
+func RunServer(port string, r *mux.Router) {
+	http.ListenAndServe(port, r)
+}
+
+// Apis - collected apis
+func Apis(r *mux.Router) {
+
+	r.HandleFunc("/method", controllers.JSONMarshal).Methods("POST")
 }
